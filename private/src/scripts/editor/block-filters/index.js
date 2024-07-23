a93d72001205f1baa554f36658087eb90b7d8ed9
GentySpun <paul.gent@bigbite.net>
2024-07-23 09:47:34 +0100

    Remove another mistakenly pushed file

--- private/src/scripts/editor/block-filters/index.js
+++
@@ -1,6 +0,0 @@
-import './admin-only-blocks';
-import './hide-unused-embeds';
-import './html-block-attributes';
-import './block-user-permissions';
-import './remove-image-block-styles';
-import './toggle-image-block-metadata.jsx';
